42861df7af1d8add42b13e96d69cb36ebfaa0d1b
Braga_Dev <168691152+DevvBragaa@users.noreply.github.com>
2024-12-08 21:54:14 -0300

    Add files via upload

---
+++ backend/src/main/kotlin/com/ecommerce/backend/EcommerceBackendKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.ecommerce.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EcommerceBackendKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<EcommerceBackendKotlinApplication>(*args)
+}
